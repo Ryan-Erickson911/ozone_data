00b3d4d30dfc849f8df4f09615216e747954da45
Ryan Erickson <ryanerickson407@gmail.com>
2023-05-31 12:01:12 -0600

    updated monthly variables

---
+++ daily_ncfile_processing.R
@@ -0,0 +1,118 @@
+path_start = "data/" #change as needed - I set my project directory to the same folder as the data
+path_final = "tiffs/"
+path_cropped = "final_data/"
+
+#Checking to make sure folders exist - creating folders if necessary
+ifelse(dir.exists(path_start),"Data folder is present",
+       "You need to download the data from GridMET and move it into the 'data' folder.")
+ifelse(dir.exists(path_final), "Folder to save files to already exists, proceeding...", dir.create(path_final))
+ifelse(dir.exists(paste0(path_final,"Daily/")), "Folder to save Daily files to already exists, proceeding...", dir.create(paste0(path_final,"Daily/")))
+ifelse(dir.exists(paste0(path_final,"Monthly/")), "Folder to save Monthly files to already exists, proceeding...", dir.create(paste0(path_final,"Monthly/")))
+ifelse(dir.exists(paste0(path_final,"Yearly/")), "Folder to save Yearly files to already exists, proceeding...", dir.create(paste0(path_final,"Yearly/")))
+ifelse(dir.exists(paste0(path_cropped,"Daily/")), "Folder to save final Daily files to already exists, proceeding...", dir.create(paste0(path_cropped,"Daily/")))
+ifelse(dir.exists(paste0(path_cropped,"Monthly/")), "Folder to save final Monthly files to already exists, proceeding...", dir.create(paste0(path_cropped,"Monthly/")))
+ifelse(dir.exists(paste0(path_cropped,"Yearly/")), "Folder to save final Yearly files to already exists, proceeding...", dir.create(paste0(path_cropped,"Yearly/")))
+ifelse(dir.exists(path_cropped), "Folder to save cropped files to already exists, proceeding...", dir.create(path_cropped))
+
+# Input Files
+input_nc = list.files(path_start)
+averages = grep("th_",input_nc, invert=TRUE, value=TRUE)
+modes = grep("th_",input_nc, value=TRUE)
+#Mapping Shapefile
+usab_prj="EPSG:4326"
+co_bound = st_as_sf(USAboundaries::us_states(states="Colorado"), crs=raster::crs(usab_prj))
+
+#Averages
+for (x in averages){
+  ncfile =  ncdf4::nc_open(paste0(path_start,x))
+  varnames = format(as.Date(ncfile$dim$day$vals, origin=as.Date("1900-01-01")),"%b.%d.%Y")
+  nc2raster = stack(paste0(path_start,x))
+  names(nc2raster) = varnames
+  nm = gsub(".nc","",x)
+  # daily = paste0(path_final,"Daily/",nm,"_daily.tiff")
+  monthly = paste0(path_final,"Monthly/",nm,"_monthly_avg.tiff")
+  yearly = paste0(path_final,"Yearly/",nm,"_yearly_avg.tiff")
+  # writeRaster(nc2raster,daily,format = 'GTiff',overwrite = TRUE)
+  
+  # crop_daily = crop(nc2raster,co_bound)
+  # output = paste0(path_cropped,"Daily/",nm,".tiff")
+  # writeRaster(crop,output,format = 'GTiff',overwrite = TRUE)
+  
+  jan_average=mean(nc2raster[[grep("Jan",names(nc2raster))]])
+  feb_average=mean(nc2raster[[grep("Feb",names(nc2raster))]])
+  mar_average=mean(nc2raster[[grep("Mar",names(nc2raster))]])
+  apr_average=mean(nc2raster[[grep("Apr",names(nc2raster))]])
+  may_average=mean(nc2raster[[grep("May",names(nc2raster))]])
+  jun_average=mean(nc2raster[[grep("Jun",names(nc2raster))]])
+  jul_average=mean(nc2raster[[grep("Jul",names(nc2raster))]])
+  aug_average=mean(nc2raster[[grep("Aug",names(nc2raster))]])
+  sep_average=mean(nc2raster[[grep("Sep",names(nc2raster))]])
+  oct_average=mean(nc2raster[[grep("Oct",names(nc2raster))]])
+  nov_average=mean(nc2raster[[grep("Nov",names(nc2raster))]])
+  dec_average=mean(nc2raster[[grep("Dec",names(nc2raster))]])
+
+  stacked_months=brick(c(jan_average,feb_average,mar_average,apr_average,may_average,jun_average,jul_average,aug_average,sep_average,oct_average,nov_average,dec_average))
+  names(stacked_months) = paste0(month.abb,"_avg")
+  writeRaster(stacked_months, monthly, format = 'GTiff', overwrite = TRUE)
+
+  crop_monthly = crop(stacked_months,co_bound)
+  output_mo = paste0(path_cropped,"Monthly/",nm,"_monthly_avg.tiff")
+  writeRaster(crop_monthly,output_mo,format = 'GTiff',overwrite = TRUE)
+
+  yearly_average=mean(nc2raster)
+  writeRaster(yearly_average,yearly,format = 'GTiff',overwrite = TRUE)
+
+  crop_yearly = crop(yearly_average,co_bound)
+  output_yr = paste0(path_cropped,"Yearly/",nm,"_yearly_avg.tiff")
+  writeRaster(crop_yearly,output_yr,format = 'GTiff',overwrite = TRUE)
+}
+
+# Modes
+
+### Need mode function that works for raster stacks
+
+# for (y in modes){
+#   ncfile =  ncdf4::nc_open(paste0(path_start,y))
+#   varnames = format(as.Date(ncfile$dim$day$vals, origin=as.Date("1900-01-01")),"%b.%d.%Y")
+#   nc2raster = stack(paste0(path_start,y))
+#   names(nc2raster) = varnames
+#   nm = gsub(".nc","",y)
+#   nm = gsub("th","wind_direction",nm)
+#   daily = paste0(path_final,"Daily/",nm,"_daily.tiff")
+#   monthly = paste0(path_final,"Monthly/",nm,"_monthly_mode.tiff")
+#   writeRaster(nc2raster,daily,format = 'GTiff',overwrite = TRUE)
+#   
+#   jan_mode=mode(nc2raster[[grep("Jan",names(nc2raster))]])
+#   feb_mode=mode(nc2raster[[grep("Feb",names(nc2raster))]])
+#   mar_mode=mode(nc2raster[[grep("Mar",names(nc2raster))]])
+#   apr_mode=mode(nc2raster[[grep("Apr",names(nc2raster))]])
+#   may_mode=mode(nc2raster[[grep("May",names(nc2raster))]])
+#   jun_mode=mode(nc2raster[[grep("Jun",names(nc2raster))]])
+#   jul_mode=mode(nc2raster[[grep("Jul",names(nc2raster))]])
+#   aug_mode=mode(nc2raster[[grep("Aug",names(nc2raster))]])
+#   sep_mode=mode(nc2raster[[grep("Sep",names(nc2raster))]])
+#   oct_mode=mode(nc2raster[[grep("Oct",names(nc2raster))]])
+#   nov_mode=mode(nc2raster[[grep("Nov",names(nc2raster))]])
+#   dec_mode=mode(nc2raster[[grep("Dec",names(nc2raster))]])
+#   stacked_months(c(jan_mode,feb_mode,mar_mode,apr_mode,may_mode,jun_mode,jul_mode,aug_mode,sep_mode,oct_mode,nov_mode,dec_mode))
+#   names(stacked_months) = paste0(month.abb,"_avg")
+#   writeRaster(stacked_months,monthly,format = 'GTiff',overwrite = TRUE)
+# }
+
+# A quick check
+test = raster(paste0(path_final,list.files(paste0(path_final)))[1])
+plot(test)
+test2 = raster(paste0(path_cropped,list.files(path_cropped)[11]))
+plot(test2, main="Mean Temperature for CO (Celsius): April 2018")
+
+plot(added_data_projected, main="Maximum Temperature")
+plot(o3_projected, add=TRUE)
+plot(den_projected, add=TRUE)
+
+plot(added_data_projected1, main="Elevation")
+plot(o3_projected, add=TRUE)
+plot(den_projected, add=TRUE)
+
+plot(roads_projected, main = "Roads Network")
+plot(den_projected, col="red", add=TRUE)
+plot(o3_projected, col="green", add=TRUE)
--- data_processing.R
+++ data_processing.R
@@ -8,94 +8,87 @@ library(stringr)
 library(raster)
 library(sf)
 library(dplyr)
+source("../geocoding_addresses/R/ozone_krige.R")
 
 # Necessary Folders
 path_start = "data/" #change as needed - I set my project directory to the same folder as the data
 # Data: http://thredds.northwestknowledge.net:8080/thredds/reacch_climate_MET_aggregated_catalog.html
-# Random Forest Variables
+path_to_data_folder="../geocoding_addresses/data/krige_data/"
+added_tiffs_list = list.files(path_to_data_folder)
 
+# Random Forest Variables
 # Spatial Variables:
-# Dist to nearest Road - 
+
+### Dist to nearest Road - 
+path_to_data_folder="../geocoding_addresses/data/krige_data/"
+added_tiffs_list = list.files(path_to_data_folder)
+path_to_roads="../geocoding_addresses/data/krige_data/co_roads_2019/"
+road_shp_path = "co_roads_2019.shp"
+# C = County
+# I = Interstate
+# M = Common Name
+# O = Other
+# S = State recognized
+# U = U.S.
+roads_shp = st_read(paste0(path_to_roads,road_shp_path))
+roads = st_transform(roads_shp,crs=CRS(prg))
+roads_projected = as(roads, "Spatial")
+dist2road = o3_projected$dist2road[1]=round(rgeos::gDistance(roads_projected,o3_projected[1,]),2)
+for(i in 2:nrow(o3_projected)){
+  dist2road=c(dist2road,round(rgeos::gDistance(roads_projected,o3_projected[i,]),2))
+}
+o3_projected$dist2road=dist2road
+
 # sum of roads in 500m buffer - 
-# lat - 
-# long - 
-# elevation -
+add_road_buffer = read_csv("../geocoding_addresses/data/roads_in_500m_buffer.csv")[,c(2,4)] %>% 
+  filter(site_name != "Aspen Park" & site_name != "Evergreen" & site_name !="Welch")
+names(add_road_buffer) = c("site_name","road_length")
+add_road_buffer$site_name=toupper(add_road_buffer$site_name)
+add_road_buffer$road_length=ifelse(is.na(add_road_buffer$road_length),0,add_road_buffer$road_length)
+
+# lat 
+o3_projected@coords[,"lat"]
+
+# long
+o3_projected@coords[,"long"]
+
+# elevation
+elevation_to_add = raster(paste0(path_to_data_folder,added_tiffs_list[2]))
+elevation_projected = raster::projectRaster(elevation_to_add, crs=prg)
+o3_projected$elev=round(raster::extract(elevation_projected,o3_projected),2)
+
 
 # Temporal Variables:
 # monthly dummy variable -
+# make a 1 for each month of interest and a 0 for other months
 # yearly dummy variable -
+# make a 1 for each year of interest and a 0 for other years
 
 # Spatio-Temporal Variables:
 # yearly NDVI - Average NDVI 500m buffer - need buffer data is ready
 # Monthly mode of wind direction
 # Monthly total precip
-
-
 # Monthly max tmep
+# added_tiffs_list -> for identifying correct tiffs
+max_temperature_to_add = raster(paste0(path_to_data_folder,added_tiffs_list[15]))
+max_temperature_projected = raster::projectRaster(max_temperature_to_add, crs=prg)
+o3_projected$max_temp=round(raster::extract(max_temperature_projected,o3_projected),2)
+#rounded K to F formula: 1.8*(K-273) + 32
+
+# head(cbind(o3_projected$site_name,o3_projected$dist2road,o3_projected$max_temp,o3_projected$elev))
+# join elevation column back to o3_projected
+# o3_projected = merge(x=o3_projected,y=o3_elevs,by="site_name", duplicateGeoms = TRUE)
+#o3_projected
+year_o3 = as.data.frame(o3_projected)
+year_o3 = year_o3 %>% 
+  dplyr::select(c("site_name","dist2road","long","lat"),everything())
+
+# use to create dataframe of specific months, ex below is summer
+# summer_o3 = year_o3 %>%
+#   dplyr::select(contains(c("site_name","lat","long","elevation","Apr","May","Jun","Jul","Aug","Sep","Oct")))
+
+# preview data
+#summer_o3
+
 # Monthly max rh 
 # Monthly wind speed
-
-path_final = "tiffs/"
-path_cropped = "final_data/"
-
-#Checking to make sure folders exist - creating folders if necessary
-ifelse(dir.exists(path_start),"Data folder is present",
-       "You need to download the data from GridMET and move it into the 'data' folder.")
-ifelse(dir.exists(path_final), "Folder to save files to exists, proceeding...", dir.create(path_final))
-ifelse(dir.exists(path_cropped), "Folder to save cropped files to exists, proceeding...", dir.create(path_cropped))
-
-# Input Files
-input_nc = list.files(path_start) # making 14 varialbes so there are 14 NetCDFs
-#Mapping Shapefile
-prj="EPSG:4326"
-co_bound = st_as_sf(USAboundaries::us_states(states="Colorado"), crs=raster::crs(prj))
-
-for (x in input_nc){
-  ncfile =  ncdf4::nc_open(paste0(path_start,x))
-  varname = names(ncfile$var)
-  nc2raster = stack(raster(paste0(path_start,x),varname = varname,band = 1))
-  nm = gsub(".nc","",x)
-  output = paste0(path_final,nm,".tiff")
-  writeRaster(nc2raster,output,format = 'GTiff',overwrite = TRUE)
-}
-
-input_tiffs = list.files(path_final)
-for (x in input_tiffs) {
-  r = raster(paste0(path_final,x))
-  crop = crop(r,co_bound)
-  nm = gsub(".tiff","",x)
-  output = paste0(path_cropped,nm,".tiff")
-  writeRaster(crop,output,format = 'GTiff',overwrite = TRUE)
-}
-
-# Temperature Conversion - 12 layer NC file
-# download from: http://thredds.northwestknowledge.net:8080/thredds/catalog/TERRACLIMATE_ALL/data/catalog.html
-
-folder_name = "TerraClim_tmax_2017-2022/" #name of the folder you moved the terra climate data into
-final_folder_name = "temperatures/" #name of the folder you moved the terra climate data into
-input_temp_ncs = list.files(paste0(path_start,folder_name)) # making 14 variables so there are 14 NetCDFs
-
-for (x in input_temp_ncs){
-  ncfile =  ncdf4::nc_open(paste0(path_start,folder_name,x))
-  varnames=format(as.Date(ncfile$dim$time$val, origin=as.Date("1900-01-01")),"%b.%Y")
-  nc2raster=stack(brick(paste0(path_start,folder_name,x)))
-  names(nc2raster)=varnames
-  nm = gsub(".nc","",x)
-  output = paste0(path_final,final_folder_name,nm,".tiff")
-  writeRaster(nc2raster,output,format = 'GTiff',overwrite = TRUE)
-}
-
-input_tiffs = list.files(paste0(path_final,final_folder_name))
-for (x in input_tiffs) {
-  r = raster(paste0(path_final,final_folder_name,x))
-  crop = crop(r,co_bound)
-  nm = gsub(".tiff","",x)
-  output = paste0(path_cropped,nm,".tiff")
-  writeRaster(crop,output,format = 'GTiff',overwrite = TRUE)
-}
-
-# A quick check
-test = raster(paste0(path_final,final_folder_name,list.files(paste0(path_final,final_folder_name)))[1])
-plot(test)
-test2 = raster(paste0(path_cropped,list.files(path_cropped)[11]))
-plot(test2, main="Mean Temperature for CO (Celsius): April 2018")
